aae5948530022e3d696af0087db5e8990e462aff
Toyan Green <toyangreen@gmail.com>
2020-05-29 20:35:24 +0100

    Exploding enemy will now move towards player using forces.
    - Functionality added to enemy to find next path points towards player and move in direction of that point using forces.
    - Navigation system added to the project.

--- Nexus/Private/ExplodingEnemy.cpp
+++ Nexus/Private/ExplodingEnemy.cpp
@@ -2,6 +2,10 @@
 
 
 #include "ExplodingEnemy.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+#include "NavigationSystem.h"
+#include "NavigationPath.h"
 
 // Sets default values
 AExplodingEnemy::AExplodingEnemy()
@@ -11,8 +15,10 @@ AExplodingEnemy::AExplodingEnemy()
 
 	// Initialise components
 	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
-	// Set so that the enemy doesn't cause a gap in the nav mesh
+	// Set so that the enemy doesn't cause a gap in the nav mesh.
 	MeshComponent->SetCanEverAffectNavigation( false);
+	// Set so that the enemy can be moved using forces.
+	MeshComponent->SetSimulatePhysics(true);
 	RootComponent = MeshComponent;
 }
 
@@ -20,10 +26,49 @@ AExplodingEnemy::AExplodingEnemy()
 void AExplodingEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	const float DistanceToTarget = (GetActorLocation() - NextPathPoint).Size();
+
+	if (DistanceToTarget <= RequiredDistanceToTarget)
+	{
+		// If the enemy is within range of the path point, get the next point.
+		NextPathPoint = GetNextPathPoint();
+	}
+	else
+	{
+		// Move to the next target.
+
+		// Calculate the direction the enemy should move in.
+		FVector MovementForceDirection = NextPathPoint - GetActorLocation();
+		MovementForceDirection.Normalize();
+
+		MeshComponent->AddForce(MovementForceDirection * MovementForce, NAME_None, bVelocityChange);
+	}
 }
 
 // Called when the game starts or when spawned
 void AExplodingEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Find the initial point to move towards.
+	NextPathPoint = GetNextPathPoint();
+}
+
+FVector AExplodingEnemy::GetNextPathPoint()
+{
+	// Get the first player in the world. **Will need to change for multiple players.**
+	ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
+
+	// Get the path from the enemy's current position to the player.
+	UNavigationPath* NavigationPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerCharacter);
+
+	if (1 < NavigationPath->PathPoints.Num())
+	{
+		// Return the second point in the navigation path.
+		return NavigationPath->PathPoints[1];
+	}
+
+	// If there is a problem getting a path point, return the enemy's current location.
+	return GetActorLocation();
 }
--- Nexus/Public/ExplodingEnemy.h
+++ Nexus/Public/ExplodingEnemy.h
@@ -23,9 +23,39 @@ protected:
 	virtual void BeginPlay() override;
 
 	/**
+	 * \brief Get the next location the enemy should move to.
+	 * \return The point location.
+	 */
+	FVector GetNextPathPoint();
+
+	/**
 	 * \brief The visible mesh of the barrel.
 	 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UStaticMeshComponent* MeshComponent;
+
+	/**
+	 * \brief The magnitude of the movement force applied to the enemy when moving in the direction of its next path point.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "ExplodingEnemy")
+	float MovementForce;
+
+	/**
+	 * \brief How close the enemy needs to be to a movement target for positions to be considered equal.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "ExplodingEnemy")
+	float RequiredDistanceToTarget;
+
+	/**
+	 * \brief Flag to set whether the movement force applied should be a change in velocity.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "ExplodingEnemy")
+	bool bVelocityChange;
+
+private:
+	/**
+	 * \brief The next location the enemy should move to.
+	 */
+	FVector NextPathPoint;
 
 };
