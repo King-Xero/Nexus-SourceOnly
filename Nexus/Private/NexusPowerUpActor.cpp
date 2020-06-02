ad9cc241df9b61ab6ebb9e06d04100a784b8d3b0
Toyan Green <toyangreen@gmail.com>
2020-06-02 18:40:15 +0100

    Create a speed boost power up.
    - Power up class now has a mesh and a light.
    - Mesh material and light colour configured through the power up class.
    - Mesh and light flash, using variables configured in the power up class.
    - Added graphics assets for the speed power up.
    - Implemented the speed power up functionality in the speed power up BP.

--- Nexus/Private/NexusPowerUpActor.cpp
+++ Nexus/Private/NexusPowerUpActor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "NexusPowerUpActor.h"
+#include "Components/PointLightComponent.h"
 
 // Sets default values
 ANexusPowerUpActor::ANexusPowerUpActor()
@@ -9,13 +10,23 @@ ANexusPowerUpActor::ANexusPowerUpActor()
  	// Power up does not need to tick.
 	PrimaryActorTick.bCanEverTick = false;
 
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = MeshComponent;
+
+	LightComponent = CreateDefaultSubobject<UPointLightComponent>(TEXT("LightComponent"));
+	// Configure default light properties.
+	LightComponent->SetCastShadows(false);
+	LightComponent->SetAttenuationRadius(100.0f);
+	LightComponent->SetupAttachment(RootComponent);
+	
 	TicksProcessed = 0;
 }
 
 void ANexusPowerUpActor::ActivatePowerUp()
 {
-	// If the effect is applied more than once, we should set a timer to repeatedly apply it.
-	if (TotalNumberOfTicks > 1)
+	// Set a timer to apply the power up effect.
+	if (PowerUpInterval > 1)
 	{
 		GetWorldTimerManager().SetTimer(TimerHandle_PowerUp, this, &ANexusPowerUpActor::PowerUpTick, PowerUpInterval, true, 0.0f);
 	}
@@ -24,25 +35,57 @@ void ANexusPowerUpActor::ActivatePowerUp()
 		// Apply the effect once.
 		PowerUpTick();
 	}
+
+	// Mesh and light should be hidden off when the power up is collected/activated.
+	MeshComponent->SetVisibility(false);
+	LightComponent->SetVisibility(false);
 }
 
 // Called when the game starts or when spawned
 void ANexusPowerUpActor::BeginPlay()
 {
-	Super::BeginPlay();	
+	Super::BeginPlay();
+
+	PowerUpMaterialInstance = MeshComponent->CreateAndSetMaterialInstanceDynamicFromMaterial(0, MeshComponent->GetMaterial(0));
+	if (PowerUpMaterialInstance)
+	{
+		// Set the color of the power up, configure the mesh material flashing.
+		PowerUpMaterialInstance->SetVectorParameterValue(MaterialInstanceColorParameterName, PowerUpColor);
+		PowerUpMaterialInstance->SetScalarParameterValue(MaterialInstanceTimeMultiplierParameterName, PowerUpFlashTimerMultiplier);
+		PowerUpMaterialInstance->SetScalarParameterValue(MaterialInstanceMinimumEmissiveParameterName, PowerUpFlashMinimumEmissive);
+	}
+		
+	LightMaterialInstance = UMaterialInstanceDynamic::Create(LightComponent->GetMaterial(0), LightComponent);
+	if (LightMaterialInstance)
+	{
+		// Configure the light flashing.
+		LightMaterialInstance->SetScalarParameterValue(MaterialInstanceTimeMultiplierParameterName, PowerUpFlashTimerMultiplier);
+		LightMaterialInstance->SetScalarParameterValue(MaterialInstanceMinimumEmissiveParameterName, PowerUpFlashMinimumEmissive);
+		LightComponent->SetMaterial(0, LightMaterialInstance);
+	}
+	// Set the light color.
+	LightComponent->SetLightColor(PowerUpColor);
 }
 
 void ANexusPowerUpActor::PowerUpTick()
 {
-	// Apply the power up effect.
-	ApplyPowerUpEffect();
+	if (0 == TicksProcessed)
+	{
+		// This should only ever get called once on the first activation.
+		OnActivated();
+	}
 	
-	if (++TicksProcessed >= TotalNumberOfTicks)
+	if (++TicksProcessed > TotalNumberOfTicks)
 	{
 		// The power up has expired, so deactivate the effect.
 		OnDeactivated();
 
 		// Clear the timer so we stop processing the power up.
 		GetWorldTimerManager().ClearTimer(TimerHandle_PowerUp);
-	}	
+	}
+	else
+	{
+		// Apply the power up effect.
+		ApplyPowerUpEffect();
+	}
 }
--- Nexus/Public/NexusPowerUpActor.h
+++ Nexus/Public/NexusPowerUpActor.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "NexusPowerUpActor.generated.h"
 
+class UPointLightComponent;
+
 UCLASS()
 class NEXUS_API ANexusPowerUpActor : public AActor
 {
@@ -47,6 +49,18 @@ protected:
 	 */
 	UFUNCTION()
 	void PowerUpTick();
+
+	/**
+	 * \brief The visible mesh of the power up.
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* MeshComponent;
+
+	/**
+	 * \brief The light surrounding the power up.
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UPointLightComponent* LightComponent;
 	
 	/**
 	 * \brief Interval between power up ticks.
@@ -60,7 +74,35 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "PowerUps")
 	int TotalNumberOfTicks = 1;
 
+	/**
+	 * \brief Color of the power.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "PowerUps")
+	FLinearColor PowerUpColor = FLinearColor::White;
+	
+	/**
+	 * \brief Multiplier used to adjust the speed of the power up flash.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "PowerUps")
+	float PowerUpFlashTimerMultiplier = 0.6f;
+
+	/**
+	 * \brief Minimum emissive value of the power up flash.
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "PowerUps")
+	float PowerUpFlashMinimumEmissive = 0.2f;
+
 private:
+
+	/**
+	 * \brief Instance of the mesh's material, required to make changes to material instance at run time.
+	 */
+	UMaterialInstanceDynamic* PowerUpMaterialInstance;
+
+	/**
+	 * \brief Instance of the light's material, required to make changes to material instance at run time.
+	 */
+	UMaterialInstanceDynamic* LightMaterialInstance;
 	
 	/**
 	 * \brief Timer used to apply power up effect when PowerUpInterval > 0;
@@ -69,4 +111,19 @@ private:
 
 	// Used to track how many times the power up effect has been applied.
 	int TicksProcessed;
+
+	/**
+	 * \brief Name of parameter used to alter the color of the power up.
+	 */
+	FName MaterialInstanceColorParameterName = "PowerUpColor";
+	
+	/**
+	 * \brief Name of the parameter used to alter the speed of the power up flash.
+	 */
+	FName MaterialInstanceTimeMultiplierParameterName = "TimeMultiplier";
+	
+	/**
+	 * \brief Name of the parameter used to set the minimum emissive value of the power up flash.
+	 */
+	FName MaterialInstanceMinimumEmissiveParameterName = "MinimumEmissiveValue";
 };
