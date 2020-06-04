85eae0b07e587018318f6738e67dd7c20d3e755f
Toyan Green <toyangreen@gmail.com>
2020-06-04 17:52:12 +0100

    Enemy wave spawning added to game mode.
    - Added wave spawning logic to the game mode class.
    - Changed the GameMode BP to use the spawn enemy event when spawning an enemy using the EQS.
    - Bound a custom event to the enemy's OnDestroyed event when spawned. When the enemy is destroyed, we run the check for any alive enemies on the game mode. If there are no alive enemies, the next wave will be prepared.
    - Changed KillZ on test map.

--- Nexus/Private/NexusGameModeBase.cpp
+++ Nexus/Private/NexusGameModeBase.cpp
@@ -1,5 +1,81 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-
 #include "NexusGameModeBase.h"
+#include "EngineUtils.h"
+#include "Components/NexusHealthComponent.h"
 
+/**
+ * \brief Start the game/match.
+ * @note Transitions to calls BeginPlay on actors.
+ */
+void ANexusGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	PrepareForNextWave();
+}
+
+void ANexusGameModeBase::StartWave()
+{
+	++WaveCount;
+	
+	// Number of enemies spawned increases with waves progressed.
+	EnemiesToSpawn = BaseEnemyCount * WaveCount;
+
+	// Start timer that repeatedly spawns enemies.
+	GetWorldTimerManager().SetTimer(TimerHandle_EnemySpawner, this, &ANexusGameModeBase::EnemySpawnerElapsed, EnemySpawnRate, true, 0.0f);
+}
+
+void ANexusGameModeBase::EndWave()
+{
+	// Stop timer that spawns enemies
+	GetWorldTimerManager().ClearTimer(TimerHandle_EnemySpawner);
+}
+
+void ANexusGameModeBase::PrepareForNextWave()
+{
+	GetWorldTimerManager().SetTimer(TimerHandle_StartNextWave, this, &ANexusGameModeBase::StartWave, WaveDelayTime);
+}
+
+void ANexusGameModeBase::CheckEnemiesAlive()
+{
+	// We should only check for alive enemies, if there are no enemies left to spawn and next wave timer is not active.
+	if (0 >= EnemiesToSpawn && !GetWorldTimerManager().IsTimerActive(TimerHandle_StartNextWave))
+	{
+		bool bEnemyAlive = false;
+
+		for (TActorIterator<APawn> PawnIterator(GetWorld()); PawnIterator; ++PawnIterator)
+		{
+			// Dereference the pawn from the iterator.
+			APawn* PawnToCheck = *PawnIterator;
+			// Check if the pawn is valid, and is not a player.
+			if (PawnToCheck && !PawnToCheck->IsPlayerControlled())
+			{
+				UNexusHealthComponent* PawnHealthComponent = Cast<UNexusHealthComponent>(PawnToCheck->GetComponentByClass(UNexusHealthComponent::StaticClass()));
+				if (PawnHealthComponent && 0.0f < PawnHealthComponent->GetCurrentHealth())
+				{
+					// Found an alive enemy, we should exit the loop immediately.
+					bEnemyAlive = true;
+					break;
+				}
+			}
+		}
+
+		if (!bEnemyAlive)
+		{
+			// If there are no alive enemies, we should prepare the next wave.
+			PrepareForNextWave();
+		}
+	}	
+}
+
+void ANexusGameModeBase::EnemySpawnerElapsed()
+{
+	// Spawn an enemy
+	SpawnNewEnemy();
+
+	if (0 >= --EnemiesToSpawn)
+	{
+		EndWave();
+	}
+}
--- Nexus/Public/NexusGameModeBase.h
+++ Nexus/Public/NexusGameModeBase.h
@@ -13,5 +13,81 @@ UCLASS()
 class NEXUS_API ANexusGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	virtual void StartPlay() override;
+
+protected:
+
+	/**
+	 * \brief Hook to spawn an enemy in blueprint.
+	 */
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewEnemy();
+
+	/**
+	 * \brief Start spawning enemies.
+	 */
+	void StartWave();
 	
+	/**
+	 * \brief Stop spawning enemies.
+	 */
+	void EndWave();
+
+	/**
+	 * \brief Set timer for next StartWave.
+	 */
+	void PrepareForNextWave();
+
+	/**
+	 * \brief Check if there are any alive enemies in the wave.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Waves")
+	void CheckEnemiesAlive();
+
+	/**
+	 * \brief The starting number of enemies used to calculate total enemies in a wave.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GameMode")
+	int BaseEnemyCount = 2;
+
+	/**
+	 * \brief Interval at which enemies are spawned.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GameMode")
+	float EnemySpawnRate = 1.0f;
+
+	/**
+	 * \brief The duration of the delay before the next wave starts.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GameMode")
+	float WaveDelayTime = 10.0f;
+
+private:
+
+	/**
+	 * \brief Callback for the timer used to spawn enemies.
+	 */
+	void EnemySpawnerElapsed();
+	
+	/**
+	 * \brief The current wave number.
+	 */
+	int WaveCount;
+	
+	/**
+	 * \brief The number of enemies to spawn in the current wave.
+	 */
+	int EnemiesToSpawn;
+	
+	/**
+	 * \brief Handle used to manage timer that spawns enemies.
+	 */
+	FTimerHandle TimerHandle_EnemySpawner;
+
+	/**
+	 * \brief Handle used to manage timer that starts the next wave.
+	 */
+	FTimerHandle TimerHandle_StartNextWave;
 };
